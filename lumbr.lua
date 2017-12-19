54bab034b3a02ea580f35b7aae0ec4f7a0241466
Adam Whitehurst <Adam.Whitehurst@live.com>
2017-12-19 14:01:50 -0800

    update

--- lumbr.lua
+++ lumbr.lua
@@ -2,7 +2,7 @@ local component = require('component')
 local geo = component.geolyzer
 
 function test ()
-	for i = 1, 6 do
+	for i = 1, 5 do
 		local analysis = geo.analyze(i)
 		print(analysis)
 	end
