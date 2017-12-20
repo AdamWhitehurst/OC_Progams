1ed9c980618aad93be4ca634b77631e1a083b466
Adam Whitehurst <Adam.Whitehurst@live.com>
2017-12-19 16:38:34 -0800

    Beep Boop
    Beep.

--- lumbr.lua
+++ lumbr.lua
@@ -5,13 +5,13 @@ local sides = require('sides')
 local geo = component.geolyzer
 local nav = component.navigation
 
-local startPos = nav.getPosition()
+local startX, startY, startZ = nil
 
 function init ()
 
 	-- TODO: Find Trunk
   -- TODO: scan tree trunk for dimensions
-
+  startX, startY, startZ = nav.getPosition()
   loop()
 end
 
@@ -19,15 +19,15 @@ function loop()
   -- TODO: checkInventory()
 
   local blocksHarvested = harvestWoodLayer()
-  -- if blocksHarvested then
-  --   if detectBlock(sides.up, 'minecraft:leaves') then
-  --     robot.swingUp()
-  --   end
+  if blocksHarvested then
+    if detectBlock(sides.up, 'minecraft:leaves') then
+      robot.swingUp()
+    end
 
-  --   moveToNextLayer()
-  -- else
-  --   returnToBase()
-  -- end
+    moveToNextLayer()
+  else
+    returnToBase()
+  end
 end
 
 -- Harvests a whole layer of the tree.
@@ -39,6 +39,8 @@ function harvestWoodLayer()
 
 	for i = 1, 4 do
 		success = harvestWoodLine()
+
+    -- TODO: Handle failures
 		robot.forward()
 		robot.turnLeft()
 	end
@@ -59,6 +61,7 @@ function harvestWoodLine()
       end
 		end
 
+    -- TODO: Handle failures
 		robot.turnRight()
 		robot.forward()
 		robot.turnLeft()
@@ -67,17 +70,10 @@ function harvestWoodLine()
   return success
 end
 
--- Returns whether a log block is in front
--- of the robot
+-- Helper function that returns whether a log
+-- block is in front of the robot
 function logFound()
-	if robot.detect() then
-		local analysis = geo.analyze(sides.front)
-		if analysis.name == 'minecraft:log' then
-			return true
-		end
-	end
-
-	return false
+	return detectBlock(sides.front, 'minecraft:log')
 end
 
 -- Once we've detected a log in front of us, try to
@@ -97,6 +93,34 @@ function moveToNextLayer()
   end
 
   return false
+end
+
+-- Returns whether the given block id is detected
+-- on the given side.
+function detectBlock(side, blockID)
+  local detectSuccessful = false
+
+  if side == sides.up then
+    detectSuccessful = robot.detectUp()
+  elseif side == sides.front then
+    detectSuccessful = robot.detect()
+  end
+
+  if detectSuccessful then
+    if analysis.name == blockID then
+      return true
+    end
+  end
+
+  return false
+end
+
+-- Go back to starting point. For now, simply
+-- moves robot down until same y-level as start.
+-- TODO: Waypoint-based movement
+function returnToBase()
+  local x, y, z = nav.getPosition()
+  while y != 
 end
 
 init()
