0df1200e21c3a3a5eefc7d37bbaceecf893dba62
Adam Whitehurst <Adam.Whitehurst@live.com>
2017-12-19 16:08:31 -0800

    WIP :+1:

--- lumbr.lua
+++ lumbr.lua
@@ -3,35 +3,71 @@ local robot = require('robot')
 local component = require('component')
 local sides = require('sides')
 local geo = component.geolyzer
+local nav = component.navigation
+
+local startPos = nav.getPosition()
 
 function init ()
-	-- Equip Axe
-	-- Find Trunk
-   harvestLoop()
+
+	-- TODO: Find Trunk
+  -- TODO: scan tree trunk for dimensions
+
+  loop()
 end
 
-function harvestLoop()
+function loop()
+  -- TODO: checkInventory()
+
+  local blocksHarvested = harvestLayer()
+  -- if blocksHarvested then
+  --   if detectBlock(sides.up, 'minecraft:leaves') then
+  --     robot.swingUp()
+  --   end
+
+  --   moveToNextLayer()
+  -- else
+  --   returnToBase()
+  -- end
+end
+
+-- Harvests a whole layer of the tree
+-- Returns whether any blocks were 
+-- successfully harvested
+function harvestLayer()
+  local success = false
+
 	for i = 1, 4 do
-		harvestWidth()
+		success = harvestWidth()
 		robot.forward()
 		robot.turnLeft()
 	end
 
-	robot.up()
+  return success
 end
 
+-- Harvests a line of logs
+-- Returns whether any blocks were
+-- successfully harvested
 function harvestWidth()
+  local success = false
+
 	for x = 0, TREE_WIDTH do
 		if logFound() then
-			harvestLog()
+			if harvestLog() then
+        success = true
+      end
 		end
 
 		robot.turnRight()
 		robot.forward()
 		robot.turnLeft()
 	end
+
+  return success
 end
 
+-- Returns whether a log block is in front
+-- of the robot
 function logFound()
 	if robot.detect() then
 		local analysis = geo.analyze(sides.front)
@@ -43,12 +79,23 @@ function logFound()
 	return false
 end
 
--- Once we've detected a log in front of us, harvest it
+-- Once we've detected a log in front of us, try to
+-- harvest it. Returns whether this was successful
 function harvestLog()
 	if robot.swing() then
     robot.suck()
+    return true
   end
+
+  return false
 end
 
+function moveToNextLayer()
+  if robot.up() then
+    return true
+  end
+
+  return false
+end
 
 init()
